328415015820ffca7feec720d7778ad907f8ed49
aya <aya@eseed.net>
2017-01-30 19:32:36 +0200

    solving conflicts and updating profile page style

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,58 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Your Home Page</title>
-	<link rel="stylesheet" type="text/css" href="{{asset('css/home_style.css')}}">
-</head>
-<body>
-<div id="logout">
-<a href="logout" style='position: relative; left: 1200px; top:20px; '>Logout</a>
-</div>
+@extends('layouts.app')
+
+@section('content')
 
 
-<div id="basic_info" width="200px" >
+<div id="basic_info" class="panel-heading">
 <h1>Basic Info</h1>
 <h2> 
 <figure> Name: {{$user->name}}</figure>
 <figure> E-mail: {{$user->email}}</figure>
 <figure>Age: {{$user->age}}</figure>
-<figure>Gender: {{$user->gender}}</figure>
-<a href="/profile/" id="updateGenres">Edit Your Prefered Genres</a><br></br>
+<figure>Gender: {{$user->gender}}</figure><br></br>
+<a href="/profile/" id="updateGenres">Edit Your Prefered Genres</a><br>
+
 <div id="update" style="display: none;">
 
   <form method="POST" action="/profile/updateGenres">
 
     {{ method_field('PATCH') }}
 
-<<<<<<< HEAD
-              <div class="panel-body">
-                <a href="/profile/myReviews" id="updateGenres">View Your Reviews</a>
-              </div>
-
-              <div class="panel-body">
-                <a href="/profile/SuggestedBooks" id="SuggestedBooks">View Suggested Books based on your preferred genre</a>
-              </div>
-
-               
-          </div>
-        </div>
-=======
     <div class="form-group">
        <textarea name="body" class="form-control">{{ $user->prefered_genre }}</textarea>
     </div>
     <div class="form-group">
        <button type="submit" class="btn btn-primary">Update Prefered Genres</button>
->>>>>>> 3136ba8b2a2cf5f82e1f3bbc3078b77aa9167e26
     </div>
   </form>
+
 </div>
-<br></br>
+
+
 <a href="/profile/myReviews" id="updateGenres">View Your Reviews</a>
 </h2>
 </div>
 
-</body>
-</html>
+@endsection
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script type="text/javascript">
--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,6 @@ Route::get('/profile/myReviews', 'ProfileController@myReviews');
 Route::get('/profile/myReviews/{review}', 'ProfileController@clickedReview');
 Route::patch('/profile/myReviews/{review}/update', 'ProfileController@updateReview');
 Route::delete('/profile/myReviews/{review}/delete', 'ProfileController@deleteReview');
-<<<<<<< HEAD
 Route::get('/profile/SuggestedBooks', 'ProfileController@suggestedBooks');
 Route::get('/home/TopBooks', 'HomeController@TopBooks');
-=======
 
-// //Route::resource('Review/{$id}/delete','ReviewController@destroy');
-// //Route::get('Review/{$id}/delete','ReviewController@destroy');
-// /*
-// Route::patch('Review/{$id}/delete','ReviewController@destroy');
-// Route::delete('Review/{$id}/delete','ReviewController@destroy');
-// */
-// Route::resource('Review','ReviewController');
->>>>>>> 3136ba8b2a2cf5f82e1f3bbc3078b77aa9167e26
